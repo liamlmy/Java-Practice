17ec2724fcccad42b1e892c06b7e3bd74721b87e
Mingyang Li <liamlmy@163.com>
2017-06-14 10:57:31 +0800

    Second version

--- Top K Frequent Words.java
+++ Top K Frequent Words.java
@@ -4,10 +4,10 @@
 // the composition is not null and is not guaranteed to be sorted
 // K >= 1 and K could be larger than the number of distinct words in the composition, in this case, 
 // just return all the distinct words
-
+//
 // Return
 // a list of words ordered from most frequent one to least frequent one (the list could be of size K or smaller than K)
-
+//
 // Examples
 // Composition = ["a", "a", "b", "b", "b", "b", "c", "c", "c", "d"], top 2 frequent words are [“b”, “c”]
 // Composition = ["a", "a", "b", "b", "b", "b", "c", "c", "c", "d"], top 4 frequent words are [“b”, “c”, "a", "d"]
