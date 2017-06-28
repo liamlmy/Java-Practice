b95d88eaa65447e557f4622434ffc20e9c35bb3f
Mingyang Li <liamlmy@163.com>
2017-06-28 17:38:09 +0800

    First version

---
+++ String Abbreviation Matching.java
@@ -0,0 +1,45 @@
+// Word “book” can be abbreviated to 4, b3, b2k, etc. Given a string and an abbreviation,
+// return if the string matches the abbreviation.
+//
+// Assumptions:
+// The original string only contains alphabetic characters.
+// Both input and pattern are not null.
+//
+// Examples:
+// pattern “s11d” matches input “sophisticated” since “11” matches eleven chars “ophisticate”.
+
+public class Solution{
+  public boolean match(String input, String pattern) {
+    return match(input, pattern, 0, 0);
+  }
+  
+  private boolean match(String input, String pattern, int i, int j) {
+    // Base case
+    // if the whole input string and pattern string are matched, return true
+    if (i == input.length() && j == pattern.length()) {
+      return true;
+    }
+    // if we run out of the input and pattern, there is still some characters left for the other one,
+    // return false
+    if (i >= input.length() || j >= pattern.length()) {
+      return false;
+    }
+    
+    // Recursion rule
+    // case 1: if the current character is not a digit
+    if (pattern.charAt(j) < '0' || pattern.charAt(j) > '9') {
+      if (pattern.charAt(j) == input.charAt(i)) {
+        return match(input, pattern, i + 1, j + 1);
+      }
+      return false;
+    }
+    // case 2: if the current character is a digit
+    // Find out the total number
+    int count = 0;
+    while (j < pattern.length() && pattern.charAt(j) >= '0' && pattern.charAt(j) <= '9') {
+      count = count * 10 + (pattern.charAt(j) - '0');
+      j++;
+    }
+    return match(input, pattern, i + count, j);
+  }
+}
