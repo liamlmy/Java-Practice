3de431c15b64a5245cf3130660bde932cae5f47a
Mingyang Li <liamlmy@163.com>
2017-07-07 16:04:51 +0800

    First version

---
+++ Largest X Of 1s.java
@@ -0,0 +1,115 @@
+// Given a matrix that contains only 1s and 0s, find the largest X shape which contains only 1s,
+// with the same arm lengths and the four arms joining at the central point.
+// Return the arm length of the largest X shape.
+//
+// Assumptions
+// The given matrix is not null, has size of N * M, N >= 0 and M >= 0.
+//
+// Examples
+// { {0, 0, 0, 0},
+//   {1, 1, 1, 1},
+//   {0, 1, 1, 1},
+//   {1, 0, 1, 1} }
+//
+// the largest X of 1s has arm length 2.
+
+public class Solution {
+  public int largest(int[][] matrix) {
+    int M = matrix.length;
+    if (M == 0) {   // Corner case
+      return 0;
+    }
+    int N = matrix[0].length;
+    if (N == 0) {   // Corner case
+      return 0;
+    }
+    int[][] up = up(matrix, M, N);
+    int[][] down = down(matrix, M, N);
+    return merge(up, down, M, N);
+  }
+  
+  private int[][] up(int[][] matrix, int M, int N) {
+    int[][] left = new int[M][N];
+    int[][] right = new int[M][N];
+    for (int i = 0; i < M; i++) {
+      for (int j = 0; j < N; j++) {
+        if (matrix[i][j] == 1) {
+          if (i == 0) {
+            left[i][j] = 1;
+            right[i][j] = 1;
+          } else if (j == 0) {
+            left[i][j] = 1;
+            if (right[i - 1][j + 1] > 0) {
+              right[i][j] = right[i - 1][j + 1] + 1;
+            } else {
+              right[i][j] = 1;
+            }
+          } else if (j == N - 1) {
+            right[i][j] = 1;
+            if (left[i - 1][j - 1] > 0) {
+              left[i][j] = left[i - 1][j - 1] + 1;
+            } else {
+              left[i][j] = 1;
+            }
+          } else {
+            if (left[i - 1][j - 1] > 0) {
+              left[i][j] = left[i - 1][j - 1] + 1;
+            } else {
+              left[i][j] = 1;
+            }
+            if (right[i - 1][j + 1] > 0) {
+              right[i][j] = right[i - 1][j + 1] + 1;
+            } else {
+              right[i][j] = 1;
+            }
+          }
+        }
+      }
+    }
+    merge(left, right, M, N);
+    return left;
+  }
+  
+  private int[][] down(int[][] matrix, int M, int N) {
+    int[][] left = new int[M][N];
+    int[][] right = new int[M][N];
+    for (int i = M - 1; i >= 0; i--) {
+      for (int j = N - 1; j >= 0; j--) {
+        if (matrix[i][j] == 1) {
+          if (i == M - 1) {
+            left[i][j] = 1;
+            right[i][j] = 1;
+          } else if (j == 0) {
+            left[i][j] = 1;
+          } else if (j == N - 1) {
+            right[i][j] = 1;
+          } else {
+            if (left[i + 1][j - 1] > 0) {
+              left[i][j] = left[i + 1][j - 1] + 1;
+            } else {
+              left[i][j] = 1;
+            }
+            if (right[i + 1][j + 1] > 0) {
+              right[i][j] = right[i + 1][j + 1] + 1;
+            } else {
+              right[i][j] = 1;
+            }
+          }
+        }
+      }
+    }
+    merge(left, right, M, N);
+    return left;
+  }
+  
+  private int merge(int[][] A, int[][] B, int M, int N) {
+    int result = 0;
+    for (int i = 0; i < M; i++) {
+      for (int j = 0; j < N; j++) {
+        A[i][j] = Math.min(A[i][j], B[i][j]);
+        result = Math.max(result, A[i][j]);
+      }
+    }
+    return result;
+  }
+}
