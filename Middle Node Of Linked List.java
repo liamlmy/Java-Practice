04335ea05f16dac211b141b922396f55dc331bec
Mingyang Li <liamlmy@163.com>
2017-07-12 19:21:52 +0800

    Second version

---
+++ Middle Node Of Linked List.java
@@ -0,0 +1,27 @@
+// Find the middle node of a given linked list.
+//
+// Examples
+// L = null, return null
+// L = 1 -> null, return 1
+// L = 1 -> 2 -> null, return 1
+// L = 1 -> 2 -> 3 -> null, return 2
+// L = 1 -> 2 -> 3 -> 4 -> null, return 2
+
+public class Solution {
+  public ListNode middleNode(ListNode head) {
+    // Corner check
+    if (head == null || head.next == null) {
+      return head;
+    }
+    // Finding the middle ListNode
+    ListNode slow = head;
+    ListNode fast = head;
+    // When fast pointer pointing to the last non-null ListNode or second last non-null ListNode,
+    // the slow pointer is pointing the middle ListNode.
+    while (fast.next != null && fast.next.next != null) {     
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+    return slow;
+  }
+}
