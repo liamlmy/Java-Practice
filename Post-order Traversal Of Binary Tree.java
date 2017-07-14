a00ac6f890af0baa3c9ea0b93285d62fc703de83
Mingyang Li <liamlmy@163.com>
2017-07-14 13:09:31 +0800

    Second version

--- Post-order Traversal Of Binary Tree.java
+++ Post-order Traversal Of Binary Tree.java
@@ -29,6 +29,10 @@
 //
 //     4
 
+/*
+ * Method 1: Recursion way
+ */
+/*
 public class Solution {
   public List<Integer> postOrder(TreeNode root) {
     List<Integer> result = new ArrayList<Integer>();
@@ -50,5 +54,81 @@ public class Solution {
     traverse(root.left, result);
     traverse(root.right, result);
     result.add(root.key);
+  }
+}
+*/
+
+/*
+ * Method 2: Iteration way with reverse pre-order.
+ */
+public class Solution {
+  public List<Integer> postOrder(TreeNode root) {
+    List<Integer> result = new ArrayList<Integer>();
+    // Corner check
+    if (root == null) {
+      return result;
+    }
+    // Traverse the tree
+    Deque<TreeNode> stack = new LinkedList<TreeNode>();
+    // Post-order is the reverse order of pre-oreder with traversing right subtree before left subtree.
+    stack.offerFirst(root);
+    while (!stack.isEmpty()) {
+      TreeNode cur = stack.pollFirst();
+      // Conduct the result for the special pre-order traversal.
+      if (cur.left != null) {
+        stack.offerFirst(cur.left);
+      }
+      if (cur.right != null) {
+        stack.offerFirst(cur.right);
+      }
+      result.add(cur.key);
+    }
+    // Reverse the pre-order traversal sequence to get the post-order traversal sequence.
+    Collections.reverse(result);
+    return result;
+  }
+}
+
+/*
+ * Method 3: Iteration way
+ */
+public class Solution {
+  public List<Integer> postOrder(TreeNode root) {
+    List<Integer> result = new ArrayList<Integer>();
+    // Corner check
+    if (root == null) {
+      return result;
+    }
+    // Traverse the tree
+    Deque<TreeNode> stack = new LinkedList<TreeNode>();
+    // Check the relation of current node and previous node to determine which direction should go next.
+    TreeNode prev = null;
+    // To record the previous node on the way of DFS so that we can determine the direction.
+    stack.offerFirst(root);
+    while (!stack.isEmpty()) {
+      TreeNode cur = stack.peekFirst();
+      // If we are going down, either left or right direction.
+      if (prev == null || cur == prev.left || cur == prev.right) {
+        // If we can still go down, try go left first.
+        if (cur.left != null) {
+          stack.offerFirst(cur.left);
+        } else if (cur.right != null) {
+          stack.offerFirst(cur.right);
+        } else {
+          // If we cannot go either way, meaning cur is a leaf.
+          result.add(cur.key);
+          stack.pollFirst();
+        }
+      } else if (prev == cur.right || prev == cur.left && cur.right == null) {
+        // If we are going up from the right side or going up from the left side but we cannot go right afterwards.
+        result.add(cur.key);
+        stack.pollFirst();
+      } else {
+        // Otherwise, we are going up from the left side and we can go down right side.
+        stack.offerFirst(cur.right);
+      }
+      prev = cur;
+    }
+    return result;
   }
 }
