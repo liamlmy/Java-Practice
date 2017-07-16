646fd2e22e1d4528c2f3b5b89b91a8b4e839e356
Mingyang Li <liamlmy@163.com>
2017-07-16 19:29:28 +0800

    First version

---
+++ Remove Certain Characters.java
@@ -0,0 +1,27 @@
+// Remove given characters in input string, the relative order of other characters should be remained.
+// Return the new string after deletion.
+//
+// Assumptions
+// The given input string is not null.
+// The characters to be removed is given by another string, it is guranteed to be not null.
+//
+// Examples
+// input = "abcd", t = "ab", delete all instances of 'a' and 'b', the result is "cd".
+
+public class Solution {
+  public String remove(String input, String t) {
+    char[] array = input.toCharArray();
+    Set<Character> set = new HashSet<Character>();
+    for (int i = 0; i < t.length(); i++) {
+      set.add(t.charAt(i));
+    }
+    int end = 0;
+    for (int i = 0; i < array.length; i++) {
+      if (!set.contains(array[i])) {
+        array[end] = array[i];
+        end++;
+      }
+    }
+    return new String(array, 0, end);
+  }
+}
