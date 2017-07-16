251d8a4632be3a9feed9408af6a3c35e050ee486
Mingyang Li <liamlmy@163.com>
2017-07-16 11:56:49 +0800

    Second version

--- All Subsets I.java
+++ All Subsets I.java
@@ -8,6 +8,10 @@
 // Set = "", all the subsets are [""]
 // Set = null, all the subsets are []
 
+/*
+ * Method 1: DFS
+ */
+/*
 public class Solution {
   public List<String> subSets(String set) {
     List<String> result = new ArrayList<String>();
@@ -34,5 +38,35 @@ public class Solution {
     int end = subSet.length();
     subSet.deleteCharAt(end - 1);
     DFS(array, result, subSet, depth + 1);
+  }
+}
+*/
+
+/*
+ * Method 2: Another DFS
+ */
+public class Solution {
+  public List<String> subSets(String set) {
+    List<String> result = new ArrayList<String>();
+    // Corner case
+    if (set == null) {
+      return result;
+    }
+    // Depth-first-searching
+    char[] array = set.toCharArray();
+    StringBuilder subSet = new StringBuilder();
+    DFS(array, result, subSet, 0);
+    return result;
+  }
+  
+  private void DFS(char[] array, List<String> result, StringBuilder subSet, int depth) {
+    result.add(subSet.toString());
+    // Maintains ascending order of the indices of picked characters.
+    // Choose the next picked index(the smallest one can be picked is index).
+    for (int i = depth; i < array.length; i++) {
+      subSet.append(array[i]);
+      DFS(array, result, subSet, i + 1);
+      subSet.deleteCharAt(subSet.length() - 1);
+    }
   }
 }
