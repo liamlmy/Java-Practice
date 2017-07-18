f07ab399277020e60a09b3592940ec1a2d3cbf76
Mingyang Li <liamlmy@163.com>
2017-07-18 17:56:40 +0800

    Problems

---
+++ ReOrder Array.java
@@ -0,0 +1,27 @@
+// Given an array of elements, reorder it as follow:
+// { N1, N2, N3, …, N2k } → { N1, Nk+1, N2, Nk+2, N3, Nk+3, … , Nk, N2k }
+// { N1, N2, N3, …, N2k+1 } → { N1, Nk+1, N2, Nk+2, N3, Nk+3, … , Nk, N2k, N2k+1 }
+// Try to do it in place.
+//
+// Assumptions
+// The given array is not null
+//
+// Examples
+// { 1, 2, 3, 4, 5, 6} → { 1, 4, 2, 5, 3, 6 }
+// { 1, 2, 3, 4, 5, 6, 7, 8 } → { 1, 5, 2, 6, 3, 7, 4, 8 }
+// { 1, 2, 3, 4, 5, 6, 7 } → { 1, 4, 2, 5, 3, 6, 7 }
+
+public class Solution {
+  public int[] reorder(int[] array) {
+    // Corner check
+    if (array == null || array.length <= 1) {
+      return array;
+    }
+    // Reorder the array
+    int i = 0;
+    int j = array.length / 2;
+    while (i < array.length / 2) {
+      
+    }
+  }
+}
