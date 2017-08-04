b56eb72933e14d1f6cee19545dcc2d336aa86913
Mingyang Li <liamlmy@163.com>
2017-08-04 14:36:50 +0800

    First version

---
+++ Search In Binary Search Tree.java
@@ -0,0 +1,26 @@
+// Find the target key K in the given binary search tree, return the node that contains the key if K is found,
+// otherwise return null.
+//
+// Assumptions
+// There are no duplicate keys in the binary search tree
+
+public class Solution {
+  public TreeNode search(TreeNode root, int key) {
+    // Corner check
+    if (root == null) {
+      return null;
+    }
+    // Find the key node
+    TreeNode current = root;
+    while (current != null) {
+      if (current.key > key) {
+        current = current.left;
+      } else if (current.key < key) {
+        current = current.right;
+      } else {
+        return current;
+      }
+    }
+    return null;
+  }
+}
