94bfc942af71eb1a3698ce6c631b61371f6e996c
Mingyang Li <liamlmy@163.com>
2017-08-14 16:45:35 +0800

    Third version

--- Stack With min().java
+++ Stack With min().java
@@ -6,6 +6,9 @@
 // top() - return the top element without remove it, if the stack is empty, return -1
 // min() - return the current min value in the stack.
 
+/*
+ * Method 1
+ */
 public class Solution {
   private Deque<Integer> stack;
   private Deque<Integer> min;
@@ -47,5 +50,63 @@ public class Solution {
       min.pollFirst();
     }
     return value;
+  }
+}
+
+/*
+ * Method 2
+ */
+public class Solution {
+  private Deque<Integer> stack;
+	private Deque<Element> min;
+
+	public Solution() {
+	    stack = new LinkedList<Integer>();
+	    min = new LinkedList<Element>();
+	  }
+
+	public int pop() {
+		if (!stack.isEmpty()) {
+			if (min.peekFirst().getValue() == stack.peekFirst()) {
+				if (min.peekFirst().getIndex() == stack.size()) {
+					min.pollFirst();
+				}
+			}
+			return stack.pollFirst();
+		}
+		return -1;
+	}
+
+	public void push(int element) {
+		stack.offerFirst(element);
+		if (min.isEmpty() || min.peekFirst().getValue() > element) {
+			min.offerFirst(new Element(element, stack.size()));
+		}
+	}
+
+	public int top() {
+		return stack.isEmpty() ? -1 : stack.peekFirst();
+	}
+
+	public int min() {
+		return stack.isEmpty() ? -1 : min.peekFirst().getValue();
+	}
+}
+
+class Element {
+  private int value;
+  private int index;
+  
+  public Element(int value, int index) {
+    this.value = value;
+    this.index = index;
+  }
+  
+  public int getValue() {
+    return value;
+  }
+  
+  public int getIndex() {
+    return index;
   }
 }
