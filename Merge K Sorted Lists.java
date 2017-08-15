746f24d5c82ae7862b1053400c2e58acf4cc6ffc
Mingyang Li <liamlmy@163.com>
2017-08-15 20:10:45 +0800

    Second version

---
+++ Merge K Sorted Lists.java
@@ -0,0 +1,35 @@
+// Merge K sorted lists into one big sorted list in ascending order.
+//
+// Assumptions
+// ListOfLists is not null, and none of the lists is null.
+
+public class Solution {
+  public ListNode merge(List<ListNode> listOfLists) {
+    PriorityQueue<ListNode> minHeap = new PriorityQueue<ListNode>(11, new MyComparator());
+    ListNode dummy = new ListNode(0);
+    ListNode cur = dummy;
+    for (ListNode node : listOfLists) {
+      if (node != null) {
+        minHeap.offer(node);
+      }
+    }
+    while (!minHeap.isEmpty()) {
+      cur.next = minHeap.poll();
+      if (cur.next.next != null) {
+        minHeap.offer(cur.next.next);
+      }
+      cur = cur.next;
+    }
+    return dummy.next;
+  }
+  
+  static class MyComparator implements Comparator<ListNode> {
+    @Override
+    public int compare(ListNode o1, ListNode o2) {
+      if (o1.value == o2.value) {
+        return 0;
+      }
+      return o1.value < o2.value ? -1 : 1;
+    }
+  }
+}
