1e7b65222595ec5fd58559a4e6b8916d279f6589
Mingyang Li <liamlmy@163.com>
2017-09-29 15:49:25 -0500

    LeetCode 160

---
+++ Intersection of Two Linked Lists.java
@@ -0,0 +1,37 @@
+// Write a program to find the node at which the intersection of two singly linked lists begins.
+//
+// Notes:
+// If the two linked lists have no intersection at all, return null.
+// The linked lists must retain their original structure after the function returns.
+// You may assume there are no cycles anywhere in the entire linked structure.
+// Your code should preferably run in O(n) time and use only O(1) memory.
+
+public class Solution {
+  public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+    int lenA = length(headA), lenB = length(headB);
+    // move headA and headB to the same start point
+    while (lenA > lenB) {
+        headA = headA.next;
+        lenA--;
+    }
+    while (lenA < lenB) {
+        headB = headB.next;
+        lenB--;
+    }
+    // find the intersection until end
+    while (headA != headB) {
+        headA = headA.next;
+        headB = headB.next;
+    }
+    return headA;
+  }
+
+  private int length(ListNode node) {
+    int length = 0;
+    while (node != null) {
+      node = node.next;
+      length++;
+    }
+    return length;
+  }
+}
