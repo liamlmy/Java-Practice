6d3a0ea8597603ad75fbec7d823e5681e245829e
Mingyang Li <liamlmy@163.com>
2017-10-05 15:22:19 -0500

    LeetCode 17

--- Letter Combinations of a Phone Number.java
+++ Letter Combinations of a Phone Number.java
@@ -3,6 +3,9 @@
 // Input:Digit string "23"
 // Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
 
+/*
+ * Method 1
+ */
 public class Solution {
   public List<String> letterCombinations(String digits) {
     if (digits == null || digits.length() == 0) {
@@ -26,5 +29,39 @@ public class Solution {
       result.add(queue.poll());
     }
     return result;
+  }
+}
+
+/*
+ * Method 2
+ */
+public class Solution {
+  final String[] keyboard = {"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+  
+  public List<String> letterCombinations(String digits) {
+    List<String> result = new ArrayList<>();
+    // Corner check
+    if (digits == null || digits.length() == 0) {
+      return result;
+    }
+    // DFS
+    StringBuilder sb = new StringBuilder();
+    DFS(result, sb, digits, 0);
+    return result;
+  }
+  
+  private void DFS(List<String> result, StringBuilder sb, String digits, int index) {
+    // Base case
+    if (index == digits.length()) {
+      result.add(new String(sb.toString()));
+      return;
+    }
+    // Recursion rule
+    String temp = keyboard[digits.charAt(index) - '0'];
+    for (int i = 0; i < temp.length(); i++) {
+      sb.append(temp.charAt(i));
+      DFS(result, sb, digits, index + 1);
+      sb.deleteCharAt(sb.length() - 1);
+    }
   }
 }
