d1058b5a28eb0cb704c73cc93f5301c79780a588
Mingyang Li <liamlmy@163.com>
2017-11-01 11:36:25 -0500

    LeetCode 50

---
+++ Pow(x, n).java
@@ -0,0 +1,19 @@
+// Implement pow(x, n).
+
+public class Solution {
+  public double myPow(double x, int n) {
+    if (n == 0) {
+      return 1;
+    }
+    if (n < 0) {
+      return myPow(1 / x, - n - 1) * 1 / x;
+    }
+    if (n == 2) {
+      return x * x;
+    }
+    if (n % 2 == 0) {
+      return myPow(myPow(x, n / 2), 2);
+    }
+    return myPow(myPow(x, n / 2), 2) * x;
+  }
+}
