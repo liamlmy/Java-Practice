d36015c2bc3ea61caf5be744c7072d15dc1e0e93
Mingyang Li <liamlmy@163.com>
2017-11-03 15:04:10 -0500

    Third version

--- Reverse Linked List In Pairs.java
+++ Reverse Linked List In Pairs.java
@@ -46,15 +46,6 @@ public class Solution {
   }
   
   // Time complexity: O(n) = O(n)
-  // Space complexity: O(n) = O(n)
+  // Space complexity: O(n) = O(1)
    */
-}
-
-class ListNode {
-  public int value;
-  public ListNode next;
-  public ListNode(int value) {
-    this.value = value;
-    next = null;
-  }
 }
