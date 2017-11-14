d96c2bc1b5ae80e2fc4c737a7e27384f8fe99fbb
Mingyang Li <liamlmy@163.com>
2017-11-13 21:15:52 -0600

    LeetCode 115

---
+++ Distinct Subsequences.java
@@ -0,0 +1,26 @@
+// Given a string S and a string T, count the number of distinct subsequences of S which equals T.
+// A subsequence of a string is a new string which is formed from the original string by deleting some (can be none) of
+// the characters without disturbing the relative positions of the remaining characters.
+// (ie, "ACE" is a subsequence of "ABCDE" while "AEC" is not).
+// Here is an example:
+// S = "rabbbit", T = "rabbit"
+// Return 3.
+
+public class Solution {
+    public int numDistinct(String s, String t) {
+        int[][] dp = new int[t.length() + 1][s.length() + 1];
+        for (int i = 0; i <= s.length(); i++) {
+            dp[0][i] = 1;
+        }
+        for (int i = 1; i <= t.length(); i++) {
+            for (int j = 1; j <= s.length(); j++) {
+                if (t.charAt(i - 1) == s.charAt(j - 1)) {
+                    dp[i][j] = dp[i][j - 1] + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = dp[i][j - 1];
+                }
+            }
+        }
+        return dp[dp.length - 1][dp[0].length - 1];
+    }
+}
