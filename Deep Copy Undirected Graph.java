4e7556126c6831e0be6417da4e0072f2982d0ef2
Mingyang Li <liamlmy@163.com>
2017-11-16 23:46:19 -0600

    Second version

--- Deep Copy Undirected Graph.java
+++ Deep Copy Undirected Graph.java
@@ -3,6 +3,9 @@
 // Assumptions
 // The given graph is not null
 
+/*
+ * Method 1: DFS
+ */
 public class Solution {
   public List<GraphNode> copy(List<GraphNode> graph) {
     // Corner check
@@ -29,5 +32,39 @@ public class Solution {
       }
       copy.neighbors.add(map.get(nei));
     }
+  }
+}
+
+/*
+ * Method 2: BFS
+ */
+public class Solution {
+  public List<GraphNode> copy(List<GraphNode> graph) {
+    // Corner check
+    if (graph == null) {
+      return null;
+    }
+    // Deep copy the graph
+    List<GraphNode> result = new ArrayList<>();
+    Map<GraphNode, GraphNode> map = new HashMap<>();
+    for (GraphNode node : graph) {
+      if (!map.containsKey(node)) {
+        map.put(node, new GraphNode(node.key));
+        Queue<GraphNode> queue = new LinkedList<>();
+        queue.offer(node);
+        while (!queue.isEmpty()) {
+          GraphNode cur = queue.poll();
+          for (GraphNode nei : cur.neighbors) {
+            if (!map.containsKey(nei)) {
+              map.put(nei, new GraphNode(nei.key));
+              queue.offer(nei);
+            }
+            map.get(cur).neighbors.add(map.get(nei));
+          }
+          result.add(map.get(cur));
+        }
+      }
+    }
+    return result;
   }
 }
