fd0fb77901e3d6c27d6fd86b70d650ab67e5d0d1
Mingyang Li <liamlmy@163.com>
2017-11-26 19:07:17 -0600

    LeetCode 542

---
+++ 01 Matrix.java
@@ -0,0 +1,65 @@
+// Given a matrix consists of 0 and 1, find the distance of the nearest 0 for each cell.
+// The distance between two adjacent cells is 1.
+// Example 1: 
+// Input:
+// 0 0 0
+// 0 1 0
+// 0 0 0
+// Output:
+// 0 0 0
+// 0 1 0
+// 0 0 0
+//
+// Example 2: 
+// Input:
+// 0 0 0
+// 0 1 0
+// 1 1 1
+// Output:
+// 0 0 0
+// 0 1 0
+// 1 2 1
+//
+// Note:
+// The number of elements of the given matrix will not exceed 10,000.
+// There are at least one 0 in the given matrix.
+// The cells are adjacent in only four directions: up, down, left and right.
+
+public class Solution {
+  public int[][] updateMatrix(int[][] matrix) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return matrix;
+        }
+        int[][] dp = new int[matrix.length][matrix[0].length];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                dp[i][j] = Integer.MAX_VALUE - 10000;       // This is very interesting here!!!!!!!!!
+            }
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    dp[i][j] = 0;
+                } else {
+                    if (i > 0) {
+                        dp[i][j] = Math.min(dp[i][j], dp[i - 1][j] + 1);
+                    }
+                    if (j > 0) {
+                        dp[i][j] = Math.min(dp[i][j], dp[i][j - 1] + 1);
+                    }
+                }
+            }
+        }
+        for (int i = matrix.length - 1; i >= 0; i--) {
+            for (int j = matrix[0].length - 1; j >= 0; j--) {
+                if (i + 1 < matrix.length) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i + 1][j] + 1);
+                }
+                if (j + 1 < matrix[0].length) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i][j + 1] + 1);
+                }
+            }
+        }
+        return dp;
+    }
+}
