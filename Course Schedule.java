33180cfdcd21afd2fdd267a4ae4241ae03c60d5c
Mingyang Li <liamlmy@163.com>
2017-11-28 23:19:31 -0600

    LeetCode 207

--- Course Schedule.java
+++ Course Schedule.java
@@ -61,40 +61,36 @@ public class Solution {
  */
 public class Solution {
   public boolean canFinish(int numCourses, int[][] prerequisites) {
-    ArrayList[] graph = new ArrayList[numCourses];
-    int[] degree = new int[numCourses];
-    Queue queue = new LinkedList();
-    int count=0;
-        
-    for(int i=0;i<numCourses;i++) {
-      graph[i] = new ArrayList();
+    List[] graph = new ArrayList[numCourses];
+    for (int i = 0; i < numCourses; i++) {
+      graph[i] = new ArrayList<Integer>();
     }
-            
-    for(int i=0; i<prerequisites.length;i++) {
-      degree[prerequisites[i][1]]++;
-      graph[prerequisites[i][0]].add(prerequisites[i][1]);
+    // indegree[i] means how many prerequisites are needed to take course i
+    int[] indegree = new int[numCourses];
+    for (int i = 0; i < prerequisites.length; i++) {
+      graph[prerequisites[i][1]].add(prerequisites[i][0]);
+      indegree[prerequisites[i][0]]++;
     }
-    for(int i=0; i<degree.length;i++) {
-      if(degree[i] == 0) {
-        queue.add(i);
-        count++;
+    int count = 0;
+    Queue<Integer> queue = new LinkedList<>();
+    // Find the entrance of the graph which are the courses that have no prerequisites
+    for (int i = 0; i < indegree.length; i++) {
+      if (indegree[i] == 0) {
+        queue.offer(i);
       }
     }
-        
-    while(queue.size() != 0) {
-      int course = (int)queue.poll();
-      for (int i = 0; i < graph[course].size(); i++) {
-        int pointer = (int)graph[course].get(i);
-        degree[pointer]--;
-        if (degree[pointer] == 0) {
-          queue.add(pointer);
-          count++;
+    while (!queue.isEmpty()) {
+      Integer cur = queue.poll();
+      count++;
+      List<Integer> nexts = graph[cur];
+      for (int i = 0; i < nexts.size(); i++) {
+        Integer nextCourse = nexts.get(i);
+        indegree[nextCourse]--;
+        if (indegree[nextCourse] == 0) {
+          queue.offer(nextCourse);
         }
       }
     }
-    if (count == numCourses) {
-      return true;
-    } else {
-      return false;
-    }
+    return count == numCourses;
+  }
 }
