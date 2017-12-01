69fe0d020ec84ae31109ac4403641e69971de74e
Mingyang Li <liamlmy@163.com>
2017-12-01 16:06:01 -0600

    LeetCode 148

---
+++ Sort List.java
@@ -0,0 +1,46 @@
+// Sort a linked list in O(n log n) time using constant space complexity.
+
+public class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        ListNode prev = null;
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        prev.next = null;
+        ListNode l1 = sortList(head);
+        ListNode l2 = sortList(slow);
+        return merge(l1, l2);
+    }
+    
+    private ListNode merge(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+                cur = cur.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+                cur = cur.next;
+            }
+        }
+        if (l1 != null) {
+            cur.next = l1;
+            cur = cur.next;
+        }
+        if (l2 != null) {
+            cur.next = l2;
+            cur = cur.next;
+        }
+        return dummy.next;
+    }
+}
