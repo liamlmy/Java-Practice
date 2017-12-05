473dea0f0d3fa4c2bcf48c94a75113b63cbe5b53
Mingyang Li <liamlmy@163.com>
2017-12-05 13:25:29 -0600

    LeetCode 41

--- First Missing Positive.java
+++ First Missing Positive.java
@@ -6,27 +6,33 @@
 
 public class Solution {
     public int firstMissingPositive(int[] nums) {
-        int length = nums.length;
-        if (length == 0) {
-            return 1;
-        }
-        int k = order(nums) + 1;
-        int temp = 0;
-        int firstMissingIndex = k;
-        for (int i = 0; i < k; i++) {
-            temp = Math.abs(nums[i]);
-            if (temp <= k) {
-                nums[temp - 1] = nums[temp - 1] < 0 ? nums[temp - 1] : -nums[temp - 1];
-            }
-        }
-        for (int i = 0; i < length; i++) {
-            if (nums[i] > 0) {
-                firstMissingIndex = i;
-                break;
-            }
-        }
-        return firstMissingIndex + 1;
-    }
+		// Corner check
+		int length = nums.length;
+		if (length == 0) {
+			return 1;
+		}
+		// Step 1: put all positive numbers to the left
+		int k = order(nums) + 1;
+		System.out.println(Arrays.toString(nums) + " k is " + k);
+		// Step 2: let all positive numbers' corresponding position of nums[] to be negative
+		int temp = 0;
+		int firstMissingIndex = k;
+		for (int i = 0; i < k; i++) {
+			temp = Math.abs(nums[i]);
+			if (temp <= k) {
+				nums[temp - 1] = nums[temp - 1] < 0 ? nums[temp - 1] : -nums[temp - 1];
+			}
+		}
+		// Step 3: scan the num[], the first element which is positive means that its index have never been exist in the num[]
+		System.out.println(Arrays.toString(nums));
+		for (int i = 0; i < length; i++) {
+			if (nums[i] > 0) {
+				firstMissingIndex = i;
+				break;
+			}
+		}
+		return firstMissingIndex + 1;
+	}
     
     private int order(int[] nums) {
         int index = -1;
