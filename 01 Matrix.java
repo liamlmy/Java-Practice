42e97bae4829f88f71e047ba14accc6766b44fde
Mingyang Li <liamlmy@163.com>
2017-12-15 19:21:22 -0600

    LeetCode 542

--- 01 Matrix.java
+++ 01 Matrix.java
@@ -27,9 +27,11 @@
 
 public class Solution {
   public int[][] updateMatrix(int[][] matrix) {
+        // Corner check
         if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
             return matrix;
         }
+        // DP
         int[][] dp = new int[matrix.length][matrix[0].length];
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
