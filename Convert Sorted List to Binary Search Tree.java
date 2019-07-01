df5cca8e52074c072a11e41fea246f2629f1ff5c
Mingyang Li <liamlmy@163.com>
2019-07-01 16:27:28 +0800

    LeetCode 109

--- Convert Sorted List to Binary Search Tree.java
+++ Convert Sorted List to Binary Search Tree.java
@@ -26,3 +26,52 @@ public class Solution {
         return node;
     }
 }
+
+
+class Solution {
+    private ListNode cur;
+
+  private int findSize(ListNode head) {
+    ListNode ptr = head;
+    int c = 0;
+    while (ptr != null) {
+      ptr = ptr.next;  
+      c += 1;
+    }
+    return c;
+  }
+
+  private TreeNode convertListToBST(int l, int r) {
+    // Invalid case
+    if (l > r) {
+      return null;
+    }
+
+    int mid = l + (r - l) / 2;
+
+    // First step of simulated inorder traversal. Recursively form
+    // the left half
+    TreeNode left = convertListToBST(l, mid - 1);
+
+    // Once left half is traversed, process the current node
+    TreeNode node = new TreeNode(cur.val);
+    node.left = left;
+
+    // Maintain the invariance mentioned in the algorithm
+    cur = cur.next;
+
+    // Recurse on the right hand side and form BST out of them
+    node.right = convertListToBST(mid + 1, r);
+    return node;
+  }
+
+  public TreeNode sortedListToBST(ListNode head) {
+    // Get the size of the linked list first
+    int size = findSize(head);
+
+    cur = head;
+
+    // Form the BST now that we know the size
+    return convertListToBST(0, size - 1);
+  }
+}
