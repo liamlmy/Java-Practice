24fc253eb6e338e51396eaacca9843692afc8c72
Mingyang Li <liamlmy@163.com>
2019-07-01 15:19:30 +0800

    LeetCode 572

---
+++ Subtree of Another Tree.java
@@ -0,0 +1,49 @@
+// Given two non-empty binary trees s and t, check whether tree t has exactly the same structure and node values with a subtree of s. A subtree of s is a tree consists of a node in s and all of this node's descendants. The tree s could also be considered as a subtree of itself.
+// Example 1:
+// Given tree s:
+//      3
+//     / \
+//    4   5
+//   / \
+//  1   2
+// Given tree t:
+//    4 
+//   / \
+//  1   2
+// Return true, because t has the same structure and node values with a subtree of s.
+// Example 2:
+// Given tree s:
+//      3
+//     / \
+//    4   5
+//   / \
+//  1   2
+//     /
+//    0
+// Given tree t:
+//    4
+//   / \
+//  1   2
+// Return false.
+
+class Solution {
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if (s == null && t == null) {
+            return true;
+        }
+        if (s == null || t == null) {
+            return false;
+        }
+        return equals(s, t) || isSubtree(s.left, t) || isSubtree(s.right, t);
+    }
+    
+    private boolean equals(TreeNode s, TreeNode t) {
+        if (s == null && t == null) {
+            return true;
+        }
+        if (s == null || t == null) {
+            return false;
+        }
+        return s.val == t.val && equals(s.left, t.left) && equals(s.right, t.right);
+    }
+}
