69ddc035bf67eb495eeb1792c7f613704abb8eb4
Mingyang Li <liamlmy@163.com>
2019-11-04 16:51:28 +0800

    LeetCode 763

---
+++ Partition Labels.java
@@ -0,0 +1,34 @@
+// A string S of lowercase letters is given. We want to partition this string into as many parts as possible so that each letter appears in at most one part, and return a list of integers representing the size of these parts.
+//
+// Example 1:
+// Input: S = "ababcbacadefegdehijhklij"
+// Output: [9,7,8]
+// Explanation:
+// The partition is "ababcbaca", "defegde", "hijhklij".
+// This is a partition so that each letter appears in at most one part.
+// A partition like "ababcbacadefegde", "hijhklij" is incorrect, because it splits S into less parts.
+//
+// Note:
+// S will have length in range [1, 500].
+// S will consist of lowercase letters ('a' to 'z') only.
+
+class Solution {
+    public List<Integer> partitionLabels(String S) {
+        int[] last = new int[26];
+        for (int i = 0; i < S.length(); i++) {
+        	last[S.charAt(i) - 'a'] = i;
+        }
+
+        List<Integer> result = new ArrayList<>();
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i < S.length(); i++) {
+        	end = Math.max(end, last[S.charAt(i) - 'a']);
+        	if (i == end) {
+        		result.add(end - start + 1);
+        		start = end + 1;
+        	}
+        }
+        return result;
+    }
+}
