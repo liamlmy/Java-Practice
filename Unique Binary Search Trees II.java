53d24debe78ba82180010550a30d1f2ccad841f5
Mingyang Li <liamlmy@163.com>
2019-11-15 11:38:55 +0800

    LeetCode 95

--- Unique Binary Search Trees II.java
+++ Unique Binary Search Trees II.java
@@ -43,3 +43,36 @@ public class Solution {
         return node;
     }
 }
+
+class Solution {
+    public List<TreeNode> generateTrees(int n) {
+    	if (n == 0) {
+    		return new ArrayList<>();
+    	}
+    	return generateTrees(1, n);
+    }
+
+    private List<TreeNode> generateTrees(int start, int end) {
+    	List<TreeNode> result = new ArrayList<>();
+
+    	if (start > end) {
+    		result.add(null);
+    		return result;
+    	}
+
+    	for (int i = start; i <= end; i++) {
+    		List<TreeNode> lefts = generateTrees(start, i - 1);
+    		List<TreeNode> rights = generateTrees(i + 1, end);
+    		for (TreeNode left : lefts) {
+    			for (TreeNode right : rights) {
+    				TreeNode root = new TreeNode(i);
+    				root.left = left;
+    				root.right = right;
+    				result.add(root);
+    			}
+    		}
+    	}
+
+    	return result;
+    }
+}
