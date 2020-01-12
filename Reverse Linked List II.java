06f3b7aae8d736f09b46cbeb3cee5bb0e74fc17d
Mingyang Li <liamlmy@163.com>
2020-01-12 11:24:19 +0800

    LeetCode 92

--- Reverse Linked List II.java
+++ Reverse Linked List II.java
@@ -7,29 +7,30 @@
 // 1 ≤ m ≤ n ≤ length of list.
 
 public class Solution {
-  public ListNode reverseBetween(ListNode head, int m, int n) {
-    // Corner check
-    if (head == null) {
-      return head;
-    }
+    public ListNode reverseBetween(ListNode head, int m, int n) {
+        // Corner check
+        if (head == null) {
+            return head;
+        }
     
-    ListNode dummy = new ListNode(0);
-    ListNode pre = dummy;               // make a pointer pre as a marker for the node before reversing
-    while (int i = 0; i < m - 1; i++) {
-    // 1 - 2 -3 - 4 - 5 ; m=2; n =4 ---> pre = 1, start = 2, then = 3
-    // dummy-> 1 -> 2 -> 3 -> 4 -> 5   
-      pre = pre.next;
+        ListNode dummy = new ListNode(0);
+        ListNode pre = dummy;            // make a pointer pre as a marker for the node before reversing
+        dummy.next = head;
+        for (int i = 0; i < m - 1; i++) {
+            // 1 - 2 -3 - 4 - 5 ; m=2; n =4 ---> pre = 1, start = 2, then = 3
+            // dummy-> 1 -> 2 -> 3 -> 4 -> 5   
+            pre = pre.next;
+        }
+        ListNode start = pre.next;          // a pointer to the beginning of a sub-list that will be reversed
+        ListNode then = start.next;         // a pointer to a node that will be reversed
+        for (int i = 0; i < n - m; i++) {
+            start.next = then.next;
+            then.next = pre.next;
+            pre.next = then;
+            then = start.next;
+        }
+        // first reversing : dummy->1 - 3 - 2 - 4 - 5; pre = 1, start = 2, then = 4
+        // second reversing: dummy->1 - 4 - 3 - 2 - 5; pre = 1, start = 2, then = 5 (finish)
+        return dummy.next;
     }
-    ListNode start = pre.next;          // a pointer to the beginning of a sub-list that will be reversed
-    ListNode then = start.next;         // a pointer to a node that will be reversed
-    for (int i = 0; i < n - m; i++) {
-      start.next = then.next;
-      then.next = pre.next;
-      pre.next = then;
-      then = start.next;
-    }
-    // first reversing : dummy->1 - 3 - 2 - 4 - 5; pre = 1, start = 2, then = 4
-    // second reversing: dummy->1 - 4 - 3 - 2 - 5; pre = 1, start = 2, then = 5 (finish)
-    return dummy.next;
-  }
 }
