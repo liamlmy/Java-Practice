f1bc034b1f03228774e2efd3867abf78bfeeb92c
Mingyang Li <liamlmy@163.com>
2020-01-12 12:04:05 +0800

    LeetCode 25

--- Reverse Nodes in k-Group.java
+++ Reverse Nodes in k-Group.java
@@ -41,28 +41,29 @@ public class Solution {
  * Method 2: Iteration way
  */
 public class Solution {
-  public ListNode reverseKGroup(ListNode head, int k) {
-    if(head == null || k == 1) {
-      return head;
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (head == null || head.next == null || k == 1) {
+            return head;
+        }
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode pre = dummy;
+        ListNode cur = head;
+        int i = 0;
+
+        while (cur != null) {
+            i++;
+            if (i % k == 0) {
+                pre = reverse(pre, cur.next);
+                cur = pre.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+
+        return dummy.next;
     }
- 
-    ListNode fake = new ListNode(0);
-    fake.next = head;
-    ListNode pre = fake;
-    int i=0;
- 
-    ListNode p = head;
-    while(p != null) {
-      i++;
-      if (i % k == 0) {
-        pre = reverse(pre, p.next);
-        p = pre.next;
-      } else {
-        p = p.next; 
-      }
-    }
-    return fake.next; 
-  }
  
 /*
  * 0->1->2->3->4->5->6
@@ -75,16 +76,17 @@ public class Solution {
  *          |  |
  *          pre next 
  */
-  public ListNode reverse(ListNode pre, ListNode next){
-    ListNode last = pre.next;
-    ListNode curr = last.next;
- 
-    while (curr != next) {
-      last.next = curr.next;
-      curr.next = pre.next;
-      pre.next = curr;
-      curr = last.next;
+    private ListNode reverse(ListNode pre, ListNode next) {
+        ListNode start = pre.next;
+        ListNode then = start.next;
+
+        while (then != next) {
+            start.next = then.next;
+            then.next = pre.next;
+            pre.next = then;
+            then = start.next;
+        }
+
+        return start;
     }
-    return last; 
-  }
 }
