b15c51abe85fa230870d60f2da1254dd8c91c178
Mingyang Li <liamlmy@163.com>
2020-01-20 15:47:53 +0800

    LeetCode 109

--- Convert Sorted List to Binary Search Tree.java
+++ Convert Sorted List to Binary Search Tree.java
@@ -75,3 +75,30 @@ class Solution {
     return convertListToBST(0, size - 1);
   }
 }
+
+class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        if (head.next == null) {
+            return new TreeNode(head.val);
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            prev = slow;
+            slow = slow.next;
+        }
+        TreeNode root = new TreeNode(slow.val);
+        if (prev != null) {
+            prev.next = null;
+        }
+        root.left = sortedListToBST(head);
+        root.right = sortedListToBST(slow.next);
+        return root;
+    }
+}
