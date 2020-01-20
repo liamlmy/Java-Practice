00e43e01e3452c59146025b4b3b0cde4a91f77b7
Mingyang Li <liamlmy@163.com>
2020-01-20 19:24:42 +0800

    LeetCode 155

--- Min Stack.java
+++ Min Stack.java
@@ -36,3 +36,36 @@ public class MinStack {
     return min;
   }
 }
+
+class MinStack {
+
+    private Stack<Integer> dataStack;
+    private Stack<Integer> minStack;
+    private int min;
+
+    public MinStack() {
+        dataStack = new Stack<>();
+        minStack = new Stack<>();
+        min = Integer.MAX_VALUE;
+    }
+
+    public void push(int x) {
+        dataStack.add(x);
+        min = Math.min(min, x);
+        minStack.add(min);
+    }
+
+    public void pop() {
+        dataStack.pop();
+        minStack.pop();
+        min = minStack.isEmpty() ? Integer.MAX_VALUE : minStack.peek();
+    }
+
+    public int top() {
+        return dataStack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
